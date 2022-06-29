c3470769f5b3799960b958ff22677ace91ad2a61
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-06-29 19:18:34 +0700

    chore: extend font family and font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,16 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      'sans': ['Exo\\ 2']
+    },
+    fontSize: {
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '5rem',
+    }
   },
   plugins: [
     require('flowbite/plugin'),
