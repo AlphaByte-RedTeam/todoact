4e8e3ee5460f6eaae305f20776f92b2b0f348133
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-07-04 14:49:56 +0700

    feat: add input column

---
+++ src/components/input.tsx
@@ -0,0 +1,39 @@
+import React, { FC, Dispatch, SetStateAction, ChangeEvent } from "react";
+
+type Props = {
+    name: string
+    value: string
+    setValue: Dispatch<SetStateAction<string>>
+}
+
+const Input: FC<Props> = ({ name, value, setValue }) => {
+    // Handling user click via submit button
+    const handleSubmit = (e: ChangeEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+        setValue(e.target.value)
+    }
+
+    // Handling user input via input column
+    const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault()
+        setValue(e.target.value)
+    }
+
+    return (
+        <div className="flex md:flex-1">
+            <input
+                type="text"
+                className={`outline-none w-96 rounded-lg`}
+                placeholder="What you want todo..."
+                name={name}
+                value={value}
+                onChange={handleInput}
+            />
+            <button className="bg-[#0EA5E9] rounded-lg mx-4 px-4" onSubmit={handleSubmit}>
+                Submit
+            </button>
+        </div>
+    )
+}
+
+export default Input;
