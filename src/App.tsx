970026376ff4deb86578c8d6074a8f61bcd20d9f
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-07-04 14:53:21 +0700

    feat: add input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import React, { FC } from 'react'
-import './App.css'
+import React, { useState, FC, FormEvent } from 'react'
+
 import Navigation from './navigation/Navigation'
+import Input from './components/input'
+import { Todo } from './components/types'
+
+import './App.css'
 
 const App: FC = () => {
+  const [todoTitle, setTodoTitle] = useState<string>('')
+
+  const handleSubmit = () => {
+    // TODO: Add code here
+  }
+  
   return (
     <div className="App">
       <header>
         <Navigation />
       </header>
+      <body className="flex md:flex-1 justify-center item-center">
+        <form onSubmit={handleSubmit}>
+          <Input name="title" value={todoTitle} setValue={setTodoTitle} />
+        </form>
+        {/* <TodoList todos={todos} setTodos={setTodos} /> */}
+      </body>
+      <footer>
+        {/* TODO: Add footer here */}
+      </footer>
     </div>
   )
 }
